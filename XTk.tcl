fffac39ab22f7c22adf32cf06f6defe6cf2624df
Patrick Sturm <siyb@geekosphere.org>
2012-04-19 20:51:48 +0200

    [ADD] added widget validation (no option validation)

--- XTk.tcl
+++ XTk.tcl
@@ -6,9 +6,32 @@ namespace eval xtk {
 	namespace import ::tcl::mathop::*
 	variable sys
 
+	# used to store a counter for each hierarchie level.
+	# will be incremented every time a widget is create.
 	set sys(pathCounter) [dict create]
+
+	# stores the most recent pack command to be invoked
+	# on child widgets. 
 	set sys(currentPackCommand) ""
+
+	# TODO: more sophisticated data structure in order to
+	# provide better code generation
+	#
+	# a list of Tk commands to be executed. Commands stored
+	# here are generated from xml
 	set sys(commandsToExecute) [list]
+
+	set sys(geomanager) [list pack place grid]
+
+	# a list of ttk widgets that is used to optain validation data
+	set sys(widgets,ttk) [list ttk::button ttk::checkbutton ttk::combobox ttk::entry ttk::frame ttk::label ttk::labelframe ttk::menubutton ttk::notebook ttk::panedwindow ttk::progressbar ttk::radiobutton ttk::scale ttk::scrollbar ttk::separator ttk::sizegrip ttk::spinbox ttk::treeview]
+
+	# a list of tk widgets that is used to obtain validation data
+	set sys(widgets,default) [list button canvas checkbutton entry frame label labelframe listbox menu menubutton message panedwindow radiobutton scale scrollbar spinbox text]
+
+	# a dict containing widget option validation data after
+	# obtainValidationData has been called
+	set sys(validation,widget,options) [dict create]
 
 	proc load {file} {
 		variable sys
@@ -56,29 +79,33 @@ namespace eval xtk {
 
 		foreach child [$element childNodes] {
 
+			set nodeName [$child nodeName]
+
+			if {$ttk} {
+				set nodeName ttk::${nodeName}
+			}
+
 			if {[isPack $child]} {
 				set sys(currentPackCommand) [getPackOptions $namespace $child]
 				traverseTree $currentPath $hierarchielevel $namespace $child $ttk
 				continue
 			} else {
+				if {![isWidgetValid $nodeName]} {
+					throwNodeErrorMessage $child "the widget '$nodeName' does not exist"
+				}
 				set parent [[$child parentNode] nodeName]
 				if {$parent ne "pack"} {
 					throwNodeErrorMessage $child "you must surround widget elements with pack elements"
 				}
 			}
 
-			set nodeName [$child nodeName]
 			set path [getUniquePathSegmentForLevel $hierarchielevel $currentPath]
 			set nodeName [$child nodeName]
 			set attributes [$child attributes]
 
 			handleVariableAttribute $namespace $path $child
 
-			if {$ttk} {
-				set tkCommand [string trim "ttk::$nodeName $path [getOptionsFromAttributes $namespace $child $attributes]"]
-			} else {
-				set tkCommand [string trim "${nodeName} $path [getOptionsFromAttributes $namespace $child $attributes]"]
-			}
+			set tkCommand [string trim "${nodeName} $path [getOptionsFromAttributes $namespace $child $attributes]"]
 
 			addToCommandList "[packTkCommand $sys(currentPackCommand) $tkCommand]"
 			# recursive -> nesting
@@ -166,6 +193,56 @@ namespace eval xtk {
 	proc throwNodeErrorMessage {node message} {
 		error "line: [$node getLine] column: [$node getColumn] -> $message"
 	}
+
+	proc obtainValidationData {} {
+		variable sys
+		set counter 0
+		foreach {key widgets} [array get sys widgets,*] {
+			foreach widget $widgets {
+				if {[catch {
+					$widget .${counter}
+					set options [.${counter} configure]
+					set supportedWidgetOptions [getAvailableOptionsFromOptionList $options]
+					destroy .${counter}
+					incr counter
+					addWidgetValidationData $widget $supportedWidgetOptions
+				} err]} {
+					puts "could not optain configuration data for $widget: $::errorInfo"
+				}
+			}
+		}
+	}
+
+	proc getAvailableOptionsFromOptionList {optionList} {
+		set ret [list]
+		foreach option $optionList {
+			foreach item $option {
+				if {[string index $item 0] eq "-"} {
+					lappend ret $item
+				} else {
+					break
+				}
+			}
+		}
+		return $ret
+	}
+
+	proc addWidgetValidationData {widget supportedWidgetOptions} {
+		variable sys
+		dict set sys(validation,widget,options) $widget $supportedWidgetOptions
+	}
+
+	proc isWidgetValid {widget} {
+		variable sys
+		return [dict exists $sys(validation,widget,options) $widget]
+	}
+
+	proc isOptionValidForWidget {widget option} {
+		variable
+		return [in $option [dict get $sys(validation,widget,options) $widget]]
+	}
 	
+	obtainValidationData
+
 	namespace export run load
 }
