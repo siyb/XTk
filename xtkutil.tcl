a023157ff772cf00de446be6a80037650e9d8184
Patrick Sturm <siyb@geekosphere.org>
2012-06-06 17:12:32 +0200

    [ADD] added return code etc ..

--- xtkutil.tcl
+++ xtkutil.tcl
@@ -7,22 +7,24 @@ if {[catch {
 	puts "Img not loaded, can't display some images"
 }
 proc generateCode {input output} {
-	if {![file exists $input]} { puts "Input file does not exist!"; return }
-	if {[file exists $output]} { puts "Output file already exists!"; return }
+	if {![file exists $input]} { puts "Input file does not exist!"; return 0 }
+	if {[file exists $output]} { puts "Output file already exists!"; return 0 }
 	set outfl [open $output w+]
 	puts $outfl [::xtk::load $input]
 	close $outfl
+	return 1
 }
 
 proc previewCode {file} {
-	if {![file exists $file]} { puts "File does not exist!"; return }
+	if {![file exists $file]} { puts "File does not exist!"; return 0 }
 	puts [::xtk::load $file]
+	return 1
 }
 
 proc preview {file} {
-	if {![file exists $file]} { puts "File does not exist!"; return }
+	if {![file exists $file]} { puts "File does not exist!"; return 0 }
 	::xtk::run [::xtk::load $file]
-	vwait foo
+	return 1
 }
 
 set command [lindex $argv 0]
@@ -33,18 +35,21 @@ if {$command eq "generate"} {
 		puts "[info script] generate ..input ..output"
 		exit 0
 	}
-	generateCode $p1 $p2
+	if {![generateCode $p1 $p2]} { exit 0 }
 } elseif {$command eq "previewcode"} {
 	if {[llength $argv] != 2} {
 		puts "[info script] previewcode ..input"
 		exit 0
 	}
-	previewCode $p1
+	if {![previewCode $p1] != 0} { exit 0 }
 } elseif {$command eq "preview"} {
 	if {[llength $argv] != 2} {
 		puts "[info script] preview ..input"
 		exit 0
 	}
-	preview $p1
+	if {![preview $p1]} { exit 0 }
+	tkwait window .
+} else {
+	puts "generate ..input ..output\npreviewcode ..input\npreview ..input"
 }
 exit 1
